c1757ac9556fb2f03cfd10ba8e6955a6165bfe55
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2020-07-21 16:05:03 +0300

    Add doc

--- command/meta.go
+++ command/meta.go
@@ -8,6 +8,7 @@ import (
 // Meta is the common options and features for all commands.
 type Meta struct{}
 
+// FatalError prints err to os.Stderr and returns a Unix exit code indicating an error.
 func (m *Meta) FatalError(err error) int {
 	fmt.Fprintf(os.Stderr, "error: %s\n", err)
 	return 1
